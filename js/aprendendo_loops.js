0a7a8453ead2d67829c9ce8dcaa1a13f9fbb829b
Kaiqui <kaiqui.lucaskaiquiluc@gmail.com>
2026-01-03 20:15:21 -0300

    estudando loops e switch

---
+++ js/aprendendo_loops.js
@@ -0,0 +1,17 @@
+// While = Quer dizer enquanto
+
+/*
+var x = 0;
+
+while (x < 10) {
+  document.write("<br> O Valor do X é: " + x);
+
+  x++;
+}
+*/
+
+// FOR = Para
+
+// for(a = 0; a < 5; a++) {
+//     document.write("<br> O Valor do A é: " + a);
+// }
