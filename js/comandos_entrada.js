13d3a275f09569cecb5af12cf74fcf1b51a70689
Kaiqui <kaiqui.lucaskaiquiluc@gmail.com>
2026-01-03 16:00:19 -0300

    primeiro commit

---
+++ js/comandos_entrada.js
@@ -0,0 +1,11 @@
+var nome = prompt("Qual é o seu nome?");
+var sobrenome = prompt("Qual é o seu sobrenome?");
+
+console.log("Olá " + nome + " " + sobrenome);
+
+document.write(`<h1> Bem vindo ao projeto, Sr(a) ${nome} ${sobrenome} </h1>`);
+
+document.write(
+  `<img src='https://sujeitoprogramador.com/steve.png' alt='Foto do Steve'/>`
+);
+
